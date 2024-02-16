c9b056088fdc3dadbcc8323ce82a2197d8deb19d
nawshad-niloy <nawshadniloy806@gmail.com>
2024-02-16 22:05:18 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {   
+        static void Greet()
+        {
+            Console.WriteLine("Good Night");
+        }
+        
+        static void Main(string[] args)
+        {
+            Greet();
+        }
+    }
+}
